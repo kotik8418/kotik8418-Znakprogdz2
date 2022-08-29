ba175f8677af0d3c2a32d2b53791f90e592af182
Kotik Andrey <Kotik8418@gmail.com>
2022-08-29 12:23:49 +0300

    Task013

---
+++ Task013/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("enter n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+if(n > 1000)
+{
+    Console.WriteLine((n / 100) % 10);
+}
+else
+{
+Console.WriteLine("маленькое число");
+}
