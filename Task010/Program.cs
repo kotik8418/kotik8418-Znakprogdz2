a8eccbc2759d0e902261ee076cfe7efe9a3b628d
Kotik Andrey <Kotik8418@gmail.com>
2022-08-29 12:14:52 +0300

    Task010

---
+++ Task010/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("enter num: ");
+int num = Convert.ToInt32(Console.ReadLine());
+            do
+            {
+                num = num / 10;
+ 
+                if (num < 100)
+                {
+                    Console.WriteLine(num.ToString().ToCharArray()[1]);
+                }
+            }
+            while (num > 100);
